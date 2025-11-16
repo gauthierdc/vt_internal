49d77686349ae33610318be62352543459464e0f
got <gauthier@de-chezelles.com>
2025-11-16 20:54:48 +0100

    Label printer and label printer logs

---
+++ vt_internal/vt_internal/doctype/label_printing_log/label_printing_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Verre & Transparence and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Label Printing Log", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ vt_internal/vt_internal/doctype/label_printing_log/label_printing_log.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, Verre & Transparence and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class LabelPrintingLog(Document):
+	pass
