f3c91a8c18e6267db03b5bd9865702e237ba982e
got <gauthier@de-chezelles.com>
2026-08-10 13:25:17 +0200

    refactor(doctypes): promeut 11 DocTypes custom en DocTypes d'app
    Les DocTypes custom ignorent les hooks doctype_js/list_js (Meta.add_code
    court-circuité). Plutôt que le bundle app_include_js, on les passe en DocTypes
    standard de l'app : leur JS client (form + list) est déplacé dans
    doctype/<nom>/ et chargé nativement par Frappe.
    
    Schéma exporté depuis la PROD (custom=0) pour éviter toute perte de champ au
    migrate (le local était en retard sur 2 doctypes).
    
    - 11 doctypes : Fiche de travail, Carte de travail VT, Quality Incident,
      Fabrication VT, BMV settings, Consolidated invoice, Order Satisfaction,
      Production statement, VT Objective, Work Completion Receipt, Quotation Approval
    - suppression du bundle vt_custom_doctypes.bundle.js (+ app_include_js)
    - testé en local : reload_doc OK, __js/__list_js bien peuplés

---
+++ vt_internal/vt_internal/doctype/work_completion_receipt/test_work_completion_receipt.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Verre & Transparence and Contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestWorkCompletionReceipt(FrappeTestCase):
+	pass
